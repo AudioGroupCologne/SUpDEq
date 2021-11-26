1fd9e90a494f0213f9816f2aa362088919aba1c0
JMA-JMA <Johannes.Arend@gmx.de>
2021-11-26 12:53:30 +0100

    Updated supdeq_optRadius

--- supdeq_optRadius.m
+++ supdeq_optRadius.m
@@ -1,6 +1,6 @@
 %% SUpDEq - Spatial Upsampling by Directional Equalization
 %
-% function r_opt = supdeq_optRadius(headWidth, headHeight, headLength)
+% function r_opt = supdeq_optRadius(headWidth, headHeight, headLength, method)
 %
 % This function calculates the optimal radius r (in m) as a linear 
 % combination of head width, head height, and head depth according to 
@@ -10,9 +10,9 @@
 % r             - Optimal radius r in m
 %
 % Input:
-% headWith      - Head with in m
+% headWidth      - Head width in m
 % headHeight    - Head height in m
-% headDepth     - Head length/depth in m
+% headLength    - Head length/depth in m
 % method        - 'Algazi' (ILD optimized) or 'Bahu' (ITD optimized)  
 %                 'Algazi' is default
 %
@@ -22,15 +22,15 @@
 % [1] - Algazi VR., Avendano C., Duda RO. - Estimation of a spherical-head 
 % model from anthropometry. J. Audio Eng. Soc. 2001; 49(6):472-479.
 %
-%?[2] H. Bahu and R. David, ?Optimization and prediction of the spherical 
+% [2] H. Bahu and R. David, ?Optimization and prediction of the spherical 
 % and ellipsoidal ITD model parameters using offset ears,? 
 % in Proceedings of the AES International Conference 
 % on Spatial Reproduction - Aesthetics and Science, 2018, pp. 1?11.
 %   
-% (C) 2018 by JMA, Johannes M. Arend
-%             TH Köln - University of Applied Sciences
-%             Institute of Communications Engineering
-%             Department of Acoustics and Audio Signal Processing
+% (C) 2018-2020 by JMA, Johannes M. Arend
+%               TH Köln - University of Applied Sciences
+%               Institute of Communications Engineering
+%               Department of Acoustics and Audio Signal Processing
 
 function r_opt = supdeq_optRadius(headWidth, headHeight, headLength, method)
 
@@ -41,13 +41,13 @@ end
 
 if strcmp(method,'Algazi')
     %r_opt = 0.51*X1 + 0.019*X2 + 0.18*X3 + 3.2 cm
-    %with X1 = head half width, X2 = head half height, X3 = head half length
+    %width X1 = head half width, X2 = head half height, X3 = head half length
     r_opt = 0.51*(headWidth/2) + 0.019*(headHeight/2) + 0.18*(headLength/2) + 0.032;
 end
 
 if strcmp(method,'Bahu')
     %r_opt = ?0.44*X1 + 0.23*X3 + 3.2 cm
-    %with X1 = head half width, X2 = head half height, X3 = head half length
+    %width X1 = head half width, X2 = head half height, X3 = head half length
     r_opt = 0.44*(headWidth/2) + 0.23*(headLength/2) + 0.032;
 end
 
